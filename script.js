04de10600dcc9b13fcb0f68177adc969833d684a
Scott <67354101+scGetStuff@users.noreply.github.com>
2022-05-20 15:06:38 -0500

    nothing specific

---
+++ script.js
@@ -0,0 +1,79 @@
+
+let myLibrary = [];
+const tableBody = document.getElementById('rows');
+const count = document.getElementById('count');
+
+
+
+function main() {
+    console.clear();
+
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+
+    myLibrary.forEach(book => addRow(book));
+    count.innerText = myLibrary.length;
+}
+
+function addBookToLibrary(title = '', author = '', pages = 0, read = false) {
+    myLibrary.push(new Book(title, author, pages, read));
+}
+
+
+function addRow(book) {
+    const row = document.createElement('tr');
+    tableBody.appendChild(row);
+    addCell(row, book.title);
+    addCell(row, book.author);
+    addCell(row, book.pages);
+    addCell(row, book.read);
+}
+
+function addCell(row, value) {
+    const cell = document.createElement('td');
+    row.appendChild(cell);
+    cell.innerText = value;
+}
+
+
+function Book(title = '', author = '', pages = 0, read = false) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.read ? 'has been read' : 'not read yet'}`;
+};
+
+
+
+main();
